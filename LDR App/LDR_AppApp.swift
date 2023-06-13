e678d25ee528eadb08c9ec58253ab57de1b32e80
rohannair183 <Rohannair183@gmail.com>
2023-06-12 18:10:45 -0600

    Initial Commit

---
+++ LDR App/LDR_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LDR_AppApp.swift
+//  LDR App
+//
+//  Created by Rohan Nair on 2023-06-12.
+//
+
+import SwiftUI
+
+@main
+struct LDR_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
